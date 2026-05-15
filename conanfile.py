1bc1c264703df42cbe06d300288f140fc674af80
Ferenc Gerlits <fgerlits@gmail.com>
2026-05-15 15:46:27 +0200

    build rocksdb with conan

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,7 @@ import shutil
 required_conan_version = ">=2.0"
 
 shared_requires = ("libarchive/3.8.7", "openssl/3.3.7", "libcurl/8.18.0", "civetweb/1.16", "libxml2/2.15.2", "fmt/12.1.0", "spdlog/1.17.0", "catch2/3.9.1", "zlib/1.3.2", "zstd/1.5.2",
-                   "bzip2/1.0.8", "lz4/1.9.4")
-
-disabled_shared_requires = ("rocksdb/10.5.1")
+                   "bzip2/1.0.8", "rocksdb/10.5.1", "lz4/1.9.4")
 
 shared_sources = ("CMakeLists.txt", "libminifi/*", "extensions/*", "minifi_main/*", "behave_framework", "bin/*", "bootstrap/*", "cmake/*", "conf/*", "controller/*", "core-framework/*",
                   "docs/*", "encrypt-config/*", "etc/*", "examples/*", "extension-framework/*", "fips/*", "minifi-api/*", "packaging/*", "thirdparty/*", "docker/*", "LICENSE", "NOTICE",
@@ -39,7 +37,7 @@ class MiNiFiCppMain(ConanFile):
         tc.variables["MINIFI_LIBCURL_SOURCE"] = "CONAN"
         tc.variables["MINIFI_OPENSSL_SOURCE"] = "CONAN"
         tc.variables["MINIFI_ZLIB_SOURCE"] = "CONAN"
-        tc.variables["MINIFI_ROCKSDB_SOURCE"] = "BUILD"  # FIXME
+        tc.variables["MINIFI_ROCKSDB_SOURCE"] = "CONAN"
         tc.variables["MINIFI_ZSTD_SOURCE"] = "CONAN"
         tc.variables["MINIFI_BZIP2_SOURCE"] = "CONAN"
         tc.variables["MINIFI_CIVETWEB_SOURCE"] = "CONAN"
